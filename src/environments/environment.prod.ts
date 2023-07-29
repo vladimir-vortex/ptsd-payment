035cc0e12a1943370c68812f7fabc90a309ab0fd
unknown <vladimir.vortex@gmail.com>
2023-07-29 11:37:08 +0300

    update 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'http://localhost:4200',
+  baseUrl: 'https://ptsd-avangraph.vercel.app',
   production: true,
   apiUrl: 'https://ptsd-rest.onrender.com',
   imagesUrl: 'https://ik.imagekit.io/dk2qinybhy'
