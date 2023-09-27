84e4bde16f8ac00c07e5f78ef12ab236118a43ed
Vortex <vladimir.vortex@gmail.com>
2023-09-28 00:21:28 +0300

    header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { TranslocoService } from '@ngneat/transloco';
 import { fadeAnimation } from './animations';
 
 @Component({
@@ -9,14 +10,26 @@ import { fadeAnimation } from './animations';
   animations: [fadeAnimation]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ptsd-test-3';
   url = '';
-  constructor(private router: Router) {
+
+  constructor(
+    private router: Router,
+    private translocoService: TranslocoService
+  ) {
     router.events.subscribe((evt) => { // will trigger each time there's a route change.
       if(evt instanceof NavigationEnd){
         this.url = evt.url;
       }
+    });
+  }
+
+  isLoading = true;
+
+  ngOnInit(): void {
+    this.translocoService.langChanges$.subscribe(() => {
+      this.isLoading = false;
     });
   }
 }
