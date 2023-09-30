a58369b1c1d3290f92a700e5f41fffa0eb54d1ac
Vortex <vladimir.vortex@gmail.com>
2023-09-30 23:28:05 +0300

    about translate

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BgTestSoundService } from 'src/app/services/bg-test-sound.service';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-about',
@@ -9,10 +10,17 @@ import { BgTestSoundService } from 'src/app/services/bg-test-sound.service';
 export class AboutComponent implements OnInit {
 
   constructor(
-    private bgTestSoundService: BgTestSoundService
+    private bgTestSoundService: BgTestSoundService,
+    private translocoService: TranslocoService
   ) { }
 
+  lang = this.translocoService.getActiveLang();
+  lang_default = this.translocoService.getDefaultLang();
+
   ngOnInit(): void {
+    this.translocoService.langChanges$.subscribe(lang => {
+      this.lang = lang;
+    });
     this.bgTestSoundService.stop();
   }
 
