bf31219edcc4118ac433a7a02cf9349ce0d22b46
vladimir-vortex <vladimir.vortex@gmail.com>
2026-03-20 10:10:58 +0200

    update

--- src/app/pages/ptsd-test-complete/ptsd-test-complete.component.ts
+++ src/app/pages/ptsd-test-complete/ptsd-test-complete.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
 import { PtsdTestService } from 'src/app/services/ptsd-test.service';
@@ -17,6 +18,7 @@ export class PtsdTestCompleteComponent {
     private router: Router,
     private route: ActivatedRoute,
     private translocoService: TranslocoService,
+    private _snackBar: MatSnackBar,
   ) {
     this.lang = this.route.snapshot.params['lang'] ?? 'en';
   }
@@ -32,9 +34,24 @@ export class PtsdTestCompleteComponent {
           this.router.navigate([this.lang, 'ptsd-test-result', response.body.testId]);
         }
       },
-      error: (error) => {
+      // error: (error) => {
+      //   this.isLoading = false;
+      //   console.error(error);
+      // },
+
+      error: (err) => {
         this.isLoading = false;
-        console.error(error);
+        const errorKey = err?.error?.message;
+      
+        const message = this.translocoService.translate(`page.auth.${errorKey}`) !== `page.auth.${errorKey}`
+          ? this.translocoService.translate(`page.auth.${errorKey}`)
+          : this.translocoService.translate('common.error.unknown');
+      
+        this._snackBar.open(message, 'OK', {
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+          duration: 3000
+        });
       },
     });
   }
